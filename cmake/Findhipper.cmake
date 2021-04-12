2bc8e31170cc41d41ba02a3caaeb1c6674f22861
Michael Howard <mphoward@auburn.edu>
2021-04-12 13:45:50 -0500

    Update copyright to 2021 (#28)

--- cmake/Findhipper.cmake
+++ cmake/Findhipper.cmake
@@ -1,3 +1,7 @@
+# Copyright (c) 2018-2020, Michael P. Howard
+# Copyright (c) 2021, Auburn University
+# This file is released under the Modified BSD License.
+
 #[=======================================================================[.rst:
 Findhipper
 ----------
